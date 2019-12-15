18fe49e414c2ffcce55478908609e8103cf4b0c3
Petrik van der Velde <petrikvandervelde@gmail.com>
2019-12-15 22:29:16 +1300

    Add the solution with the console and the entrypoint apps
    references #1

---
+++ src/sln/Test.Unit.Console/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Test.Unit.Console
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/sln/nBuildKit.Console.EntryPoint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace nBuildKit.Console.EntryPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
